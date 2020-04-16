2702f9ad7cf5ddf1b14d2c0a9adfc4ed22b707a5
CorreianAngel <43967709+CorreianAngel@users.noreply.github.com>
2020-04-16 13:04:37 -0700

    generated a test question and answer.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function generate()
+function startQuiz()
 {
    console.log("Button Pressed. First question generated."); 
-   //struggle need more time and reasearch
+   var JSON = 
+   {
+       q: "This is a test question, did it work?",
+       option1: "yes",
+       option2: "no",
+       answer: "yes"
+   };
+   var questions = JSON.q;
+   var answer = JSON.option1;
+   var answer2 = JSON.option2;
+   document.getElementById("question").innerHTML = questions;
+   document.getElementById("optt1").innerHTML = answer;
+   document.getElementById("optt2").innerHTML = answer2;
 }
+
+
 
